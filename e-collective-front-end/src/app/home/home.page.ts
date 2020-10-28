1c86ddfa2c89c75af443fc72098d97aa925d8fb9
Mariano Soto <mariano.soto.777@gmail.com>
2020-10-27 23:31:31 -0600

    Footer, Toolbar, Sidebar
    Se creo principio de la plantilla principal

--- e-collective-front-end/src/app/home/home.page.ts
+++ e-collective-front-end/src/app/home/home.page.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  private dynamicColor: string;
+  constructor() {
+    this.dynamicColor = 'light';
+  }
 
 }
