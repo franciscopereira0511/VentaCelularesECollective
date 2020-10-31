650f572ebeab15bd3ddb00f16378e65ab6a71de3
Francisco Pereira <franciscopereira0511@gmail.com>
2020-10-30 21:09:35 -0600

    Se completó el componente que muestra los dispositivos en home

--- e-collective-front-end/src/app/home/home.page.ts
+++ e-collective-front-end/src/app/home/home.page.ts
@@ -9,15 +9,15 @@ import { Device } from 'src/models/device.model';
 export class HomePage {
   private dynamicColor: string;
   devices: Device[] = [
-    {name: 'Iphone', model: '9', price: 284000, photoURL: ''},
-    {name: 'Samsung', model: 'Galaxy 4', price: 128000, photoURL: ''},
-    {name: 'Huaweii', model: 'P30', price: 250000, photoURL: ''},
-    {name: 'Samsung', model: 'a5', price: 194000, photoURL: ''},
-    {name: 'Iphone', model: '9', price: 288000, photoURL: ''},
-    {name: 'Iphone', model: '7', price: 235770, photoURL: ''},
-    {name: 'Huaweii', model: 'P40', price: 345678, photoURL: ''},
-    {name: 'Samsung', model: 'a7', price: 232300, photoURL: ''},
-    {name: 'Iphone', model: 'Galaxy 6', price: 284900, photoURL: ''}
+    {name: 'Iphone', model: '9', price: 284.000, photoURL: '../../assets/images/Iphone.jpg'},
+    {name: 'Samsung', model: 'Galaxy 4', price: 128.000, photoURL: '../../assets/images/Samsung.jpg'},
+    {name: 'Huaweii', model: 'P30', price: 250.000, photoURL: '../../assets/images/Huaweii.jpg'},
+    {name: 'Samsung', model: 'a5', price: 194.000, photoURL: '../../assets/images/Samsung.jpg'},
+    {name: 'Iphone', model: '9', price: 288.000, photoURL: '../../assets/images/Iphone.jpg'},
+    {name: 'Iphone', model: '7', price: 235.770, photoURL: '../../assets/images/Iphone.jpg'},
+    {name: 'Huaweii', model: 'P40', price: 345.678, photoURL: '../../assets/images/Huaweii.jpg'},
+    {name: 'Samsung', model: 'a7', price: 232.300, photoURL: '../../assets/images/Samsung.jpg'},
+    {name: 'Iphone', model: 'Galaxy 6', price: 284.900, photoURL: '../../assets/images/Iphone.jpg'}
   ];
 
 
